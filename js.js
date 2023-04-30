302c7e5b3409a4fc129111fb67bb4e5c36c3e89b
A-alturki <aboody3553@outlook.com>
2023-05-01 00:26:25 +0300

    made playround function

--- js.js
+++ js.js
@@ -12,4 +12,41 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if(playerSelection == computerSelection){
+        return `both ${playerSelection}! draw`;
+    }
+    else if(playerSelection == "rock" && computerSelection == "paper"){
+        return "You lose! paper beats rock";
+    }
+    else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "You win! rock beats scissors";
+    }
+    else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "You win! paper beats rock";
+    }
+    else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "You lose! scissors beats paper";
+    }
+    else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "You win! scissors beats paper";
+    }
+    else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return "You lose! rock beats scissors";
+    }
+}
+
+function game(){
+    playerWin = 0;
+    computerWin = 0;
+
+    while(computerWin != 5 ||playerWin != 5){
+        let player = windows.prompt("pick your poison...");
+        let pc = getComputerChoice();
+
+        playRound(player, pc);
+    }
+    if()
+}
